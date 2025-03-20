778f9f37d994698bf88f7f91aff0141a40dd22a0
Giuseppe Rizza <ilmoro86@hotmail.it>
2025-03-20 22:43:52 +0100

    [feat] Add Milestone 2 last day

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DefaultLayout from "./layouts/DefaultLayout";
 import HomePage from "./pages/HomePage";
 import MoviePage from "./pages/MoviePage";
+import CreateMoviePage from './pages/CreateMoviePage';
 
 import './App.css'
 
@@ -14,6 +15,7 @@ function App() {
         <Route element={<DefaultLayout />} >
           <Route index path="/" element={<HomePage />} />
           <Route path="movies/:id" element={<MoviePage />} />
+          <Route path="movies/create" element={<CreateMoviePage />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,6 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClapperboard } from '@fortawesome/free-solid-svg-icons';
 
 export default function Header() {
 
@@ -7,6 +9,9 @@ export default function Header() {
         <nav className="navbar bg-warning mb-2">
             <div className="container-fluid justify-content-start">
                 <Link className="navbar-brand" to="/">Bool Movies</Link>
+                <Link to="/movies/create" className="icon-link">
+                    <FontAwesomeIcon icon={faClapperboard} size="2x" style={{ color: "black", transform: "rotate(-10deg)" }} title="Add Movie" />
+                </Link>
             </div>
         </nav>
 
